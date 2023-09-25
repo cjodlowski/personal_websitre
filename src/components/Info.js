e359201c582efd98cc907ba889f5db8564451d5c
Claudia Jodlowski <47677786+cjodlowski@users.noreply.github.com>
2023-09-24 21:48:57 -0400

    caret styles

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { useState } from "react";
 
 import '../vendors/bootstrap/css/bootstrap.min.css'
 import '../vendors/fontawesome/css/all.min.css'
