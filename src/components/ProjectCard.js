3213a5ed8ea860039f4d76c7658d1a738a0c9e99
Jodlowski Claudia <claudiajodlowski@ups.com>
2023-11-28 13:54:39 -0500

    ugh

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -12,7 +12,7 @@ function ProjectCard({item}) {
         <>
             <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/devicons/devicon@v2.15.1/devicon.min.css"></link>
             {/* Desktop View - What We Had Before */}
-            <div className={`card override-bs mb-3 mx-2 d-none d-lg-block`}>
+            <div className={`card card-width-lvp override-bs mb-3 mx-2 d-none d-lg-block`}>
                 <h3 className="card-accent pl-2 pb-1 m-0">
                     <span className=" card-title text-break sans-serif ml-2 my-2 hover-underline-animation">{item.title}</span>
                     {item.icons.map((icon) => {
@@ -37,7 +37,7 @@ function ProjectCard({item}) {
             </div>
 
             {/* Mobile View- Title Only */}
-            <div className={`mb-2 mx-2 d-lg-none   ${showDetails ? "d-none" : ""}`}>
+            <div className={` card-width-svp mb-2 mx-2 d-lg-none   ${showDetails ? "d-none" : ""}`}>
                 <h3 className="card-accent card-mini-header  text-break sans-serif pl-2 pb-1 m-0" onClick={() => {
                         setShowDetails(!showDetails)
                     }} tabindex={0}>
@@ -45,7 +45,7 @@ function ProjectCard({item}) {
                     <span className="card-title text-break sans-serif ml-2 my-2 hover-underline-animation">{item.title}</span>
                     <div className="card-icons-div">
                         {item.icons.map((icon) => {
-                            return <i className={`${icon} card-icons-mobile d-inline`}></i>
+                            return <i className={`${icon} card-icons d-inline`}></i>
                         })}
                     </div>
 
@@ -53,7 +53,7 @@ function ProjectCard({item}) {
             </div>
 
             {/* Mobile View - Full Card Details*/}
-            <div className={`card card-mobile override-bs mb-3 mx-2 d-lg-none ${!showDetails ? "d-none" : ""}`}>
+            <div className={`card card-width-svp override-bs mb-3 mx-2 d-lg-none ${!showDetails ? "d-none" : ""}`}>
                 <h3 className="card-accent pl-2 pb-1 m-0">
                     <i className ={`fas fa-solid fa-caret-down card-caret ${showDetails ? "rotate-down" : "rotate-right"}`} onClick={() => {
                         setShowDetails(!showDetails)
@@ -61,16 +61,16 @@ function ProjectCard({item}) {
                     <span className="card-title text-break sans-serif ml-2 my-2 hover-underline-animation">{item.title}</span>
                     <div className="card-icons-div">
                         {item.icons.map((icon) => {
-                            return <i className={`${icon} card-icons-mobile d-inline`}></i>
+                            return <i className={`${icon} card-icons d-inline`}></i>
                         })}
                     </div>
 
                 </h3>
                 <hr className="card-line-deco"></hr>
                 <div className="">
-                    <img src={item.screenshot} alt="logo"className="img-border card-img-style-mobile"></img>
+                    <img src={item.screenshot} alt="logo"className="img-border card-img-style"></img>
                 </div>
-                <div className="card-body-mobile override-bs">
+                <div className="card-body override-bs">
                     <h5 className="card-text serif">{item.summary}</h5>
                     <ul className="m-0">
                         {item.bullets.map((bullet) => {
